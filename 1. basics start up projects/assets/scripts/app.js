ac957cfd0cd885c7de6f977beafc57df8ceca38c
faha1999 <kafaha1999@gmail.com>
2021-03-05 00:22:27 +0600

    2nd day

--- 1. basics start up projects/assets/scripts/app.js
+++ 1. basics start up projects/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert ('This works!')
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
