8da8295b6ca01472c92af00e0d15a7f53616983d
faha1999 <kafaha1999@gmail.com>
2021-04-15 03:51:18 +0600

    complete 23

--- 1. basics start up projects/assets/scripts/app.js
+++ 1. basics start up projects/assets/scripts/app.js
@@ -1,16 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    alert('The result is ' + result);
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-add(1, 2);
-add(5, 5);
- 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add)
