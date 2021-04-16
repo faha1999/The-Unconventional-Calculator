b3e68e9a1b629a082836c8d4d1cfdf68cfb965f0
faha1999 <kafaha1999@gmail.com>
2021-04-16 22:27:27 +0600

    ccomplete 32

--- 1. basics start up projects/assets/scripts/app.js
+++ 1. basics start up projects/assets/scripts/app.js
@@ -1,9 +1,46 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// gets input from input field
+function getUserNumberInput() {
+    return parseInt(usrInput.value);
+}
+
+// generatrs and writes calculator log
+function creatAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    creatAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    creatAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    creatAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    creatAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
